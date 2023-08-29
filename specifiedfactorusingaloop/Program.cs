da0db15434eccbdd57d5acd7580a98291c39f991
hp <hp@RAVIBAGHEL>
2023-08-29 22:16:43 +0530

    Add project files.

---
+++ specifiedfactorusingaloop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int factor = 2;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] *= factor;
+        }
+
+        Console.WriteLine("Array after multiplication: " + string.Join(", ", array));
+    }
+}
